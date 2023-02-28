9edd3146d17ad8e59d9544819e052e9ba3e8a247
Bruno <bruno.valalves@gmail.com>
2023-02-27 22:02:16 -0300

    criação de botoes

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ kivy.require('2.1.0') # replace with your current kivy version !
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 class InsertScreen(Screen):
     def __init__ (self,**kwargs):
@@ -14,7 +16,16 @@ class InsertScreen(Screen):
 class PlotScreen(Screen):
     def __init__ (self,**kwargs):
         super().__init__(**kwargs)
-        self.add_widget(Label(text ='plot screen'))
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(Label(text ='titulo'))
+        layout.add_widget(Label(text ='grafico'))
+        layout.add_widget(Button(text = 'Insert',on_press = self.go_to_insert_screen))
+        self.add_widget (layout)
+    def go_to_insert_screen(self, *args):
+        self.manager.current = 'insert'
+
+
+        
 
 class ScreenManagement(ScreenManager):
     pass
@@ -23,8 +34,8 @@ class MyApp(App):
 
     def build(self):
         screen_manager = ScreenManagement()
-        screen_manager.add_widget(InsertScreen(name = 'insert'))
         screen_manager.add_widget(PlotScreen(name = 'plot'))
+        screen_manager.add_widget(InsertScreen(name = 'insert'))
         return screen_manager
 
 
